0392e0b322886042cf38058a418d3e1ff0c63df3
Jim de Beer <jim@vigour.io>
2016-12-18 19:12:11 +0100

    correct rollup

--- rollup/browser.js
+++ rollup/browser.js
@@ -1,8 +1,9 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+// import buble from 'rollup-plugin-buble'
 
 export default {
-  entry: './wip/index.js',
+  entry: 'lib/index.js',
   plugins: [
     resolve({
       jsnext: true,
@@ -11,21 +12,12 @@ export default {
     }),
     commonjs()
   ],
-  sourceMap: true,
-  // external: [
-  //   'brisky-stamp',
-  //   'monotonic-timestamp',
-  //   'quick-hash',
-  //   'uws'
-  // ],
   targets: [
+    // { dest: 'dist/test/index.js', format: 'cjs' },
+    // { dest: 'dist/test/index.es.js', format: 'es' },
     {
-      dest: 'wip/dist/index.browser.dev.js',
-      format: 'iife',
-      moduleName: 'wip',
-      intro: `
-        var global = window;
-      `
+      dest: 'dist/index.browser.js',
+      format: 'es'
     }
   ]
 }
