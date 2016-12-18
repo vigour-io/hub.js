349d7e3679e717e4d8158284e935c82705473d4a
Jim de Beer <jim@vigour.io>
2016-12-18 12:35:41 +0100

    multiple upstreams

--- test/index.js
+++ test/index.js
@@ -1,13 +1,31 @@
 import hub from '../lib'
 
 const client = hub({
-  url: 'ws://localhost:6060'
+  url: 'ws://localhost:6060',
+  id: 'client',
+  nested: {
+    field: {
+      url: 'ws://localhost:6061'
+    }
+  }
 })
 
 setTimeout(() => {
-  const server = hub({ port: 6060 })
+  const server2 = hub({
+    port: 6061,
+    id: 'server2'
+  })
+
+  const server = hub({
+    port: 6060,
+    id: 'server'
+  })
 }, 100)
 
 client.set({
   hello: true
+})
+
+client.nested.field.set({
+  haha: true
 })
