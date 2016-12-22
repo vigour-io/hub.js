f9a80878893e985ff5713501a944c62b8aa2e9d8
Jim de Beer <jim@vigour.io>
2016-12-22 13:37:32 +0100

    correct caching

--- test/multiple.js
+++ test/multiple.js
@@ -37,11 +37,12 @@ test('client - multiple', t => {
     // console.log('client', t.path(), type)
   })
 
+  console.log('\n\nSET BLURF')
   client.set({ blurf: 'hello' })
 
   // // url: 'ws://localhost:6060'
   client.connected.once(true).then(() => {
-    console.log('go go go')
+    console.log('🐵 clientIsConnected 🐵')
     hybrid.set({
       url: 'ws://localhost:6060'
     })
@@ -50,10 +51,11 @@ test('client - multiple', t => {
   server.get('blurf', {}).once('hello', () => {
     console.log('\n🐵 !!!omfg!!! 🐵')
     // client.set({ blurf: 'x' })
+    console.log('\n\n\n---->SET SERVER')
     server.set({ blarf: 'yyy' })
 
     client.get('blarf', {}).once('yyy', () => {
-      console.log('recieved blarf!')
+      console.log('??????? recieved blarf!')
       server.set({ somefield: null })
       // client.set(null)
     })
