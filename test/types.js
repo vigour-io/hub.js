278b2af8e5b0bb8a600dde79b54bb41f1426ae88
Jim de Beer <jim@vigour.io>
2017-01-14 12:51:41 +0100

    start of syncable types field

--- test/types.js
+++ test/types.js
@@ -7,9 +7,11 @@ test('types', t => {
     port: 6060,
     somefield: 'somefield!',
     types: {
-      rick: { hello: true }
+      rick: { hello: true },
+      blurf: { bye: true, x: true }
     },
     bla: {
+      // setting val will not sync fields when using val: true this is a limit currently in the hub -- can be changed
       type: 'rick'
     }
   })
@@ -19,13 +21,19 @@ test('types', t => {
     url: 'ws://localhost:6060'
   })
 
-  client.subscribe({ bla: { val: true, type: true } }, (t) => {
+  client.subscribe({ bla: { val: true, type: true } }, t => {
     console.log('--->', t.path())
   })
 
   setTimeout(() => {
-    console.log(scraper.bla.keys())
-    console.log(!!client.bla, client.bla.keys())
+    console.log('scraper bla keys:', scraper.bla.keys())
+    console.log('client bla keys:', client.bla.keys(), client.bla.type.val)
+    // client.set({ bla: { type: 'blurf' } }) -- brings extra complexity (recursive update of nested fields using a type thats does not exist yet)
+    // console.log('client bla keys (after switch)', client.bla.keys(), client.bla.type.val)
+    // setTimeout(() => {
+    //   console.log('???', scraper.bla.keys(), scraper.bla.type.val)
+    //   console.log(client.bla.keys())
+    // }, 100)
   }, 100)
 
   t.end()
