d721988620d0c24b58c76cc852f8d55b0b2ff98d
Jim de Beer <jim@vigour.io>
2017-01-25 15:18:01 +0100

    remove debug Please enter the commit message for your changes. Lines starting

--- src/client/create.js
+++ src/client/create.js
@@ -5,7 +5,6 @@ export default (t, id, val, stamp, useragent) => {
   val.device = useragent.device
   val.platform = useragent.platform
   val.browser = useragent.browser
-  val.debug = val.context || 'no-context'
   t.set({ clients: { [id]: val } }, stamp)
   return t.clients[id]
 }
