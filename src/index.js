be31c29ed50f1127883dd522579751a1aff3e075
Jim de Beer <jim@vigour.io>
2017-01-26 15:11:17 +0100

    remove uid from default fn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import hub from './hub'
-import { pid } from './server/cache'
 const fn = (val, stamp) => hub.create(val, stamp)
-fn.pid = pid
 export default fn
 
 // add uids to stamps else it sucks -- dont compromise for tests think of that as an after thought
