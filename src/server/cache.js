1cc292e45bf5975e6ce76b698915c3b39a2a3dc2
Jim de Beer <jim@vigour.io>
2017-01-26 15:41:38 +0100

    correct cache hashing

--- src/server/cache.js
+++ src/server/cache.js
@@ -6,7 +6,7 @@ const pid = t => {
   while (p) {
     let key = p.key
     if (key !== void 0) {
-      id = id * 33 ^ hash(key)
+      id = id * 33 ^ (hash(key))
       p = p._cLevel === 1 ? p._c : p._p
     } else {
       return id >>> 0
@@ -15,6 +15,8 @@ const pid = t => {
   return id >>> 0
 }
 
+// const pid = t => t.path().join('')
+
 const cache = (client, struct, stamp) => {
   if (!client.cache) client.cache = {}
   client.cache[pid(struct)] = stamp
@@ -22,5 +24,7 @@ const cache = (client, struct, stamp) => {
 
 const isCached = (client, struct, stamp) => client.cache &&
   client.cache[pid(struct)] === stamp
+
+// const isCached = () => false
 
 export { cache, isCached, pid }
