8432cf6b741068f70f58b50c5ce435336b294aef
Jim de Beer <jim@vigour.io>
2017-02-17 14:42:42 +0100

    browser incoming listener

--- src/client/index.js
+++ src/client/index.js
@@ -228,8 +228,9 @@ const define = {
 const enableIncomingListener = (socket, hub) => {
   if (!socket.incomingOverride) {
     socket.incomingOverride = true
-    const msg = hub.socket.internalOnMessage
-    socket.internalOnMessage = (data) => {
+    const field = typeof window === 'undefined' ? 'internalOnMessage' : 'onmessage'
+    const msg = hub.socket[field]
+    socket[field] = (data) => {
       hub.emit('incoming', data)
       msg(data)
     }
@@ -240,6 +241,7 @@ const on = {
   data: { send },
   props: {
     incoming: (t, val, key, stamp) => {
+      console.log('incoming!????')
       const hub = t._p
       if (hub.socket) enableIncomingListener(hub.socket, hub)
       return emitterProperty(t, val, key, stamp)
