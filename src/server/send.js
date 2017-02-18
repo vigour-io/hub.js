1c0309bc3e4106fe7934afe9b260787005593466
Jim de Beer <jim@vigour.io>
2017-02-18 20:09:47 +0100

    remove logging size function

--- src/server/send.js
+++ src/server/send.js
@@ -9,17 +9,17 @@ const isEmpty = obj => {
   return true
 }
 
-const size = str => {
-  // returns the byte length of an utf8 string
-  var s = str.length
-  for (var i= str.length - 1; i >= 0; i--) {
-    var code = str.charCodeAt(i)
-    if (code > 0x7f && code <= 0x7ff) { s++ }
-    else if (code > 0x7ff && code <= 0xffff) { s += 2 }
-    if (code >= 0xDC00 && code <= 0xDFFF) i-- // trail surrogate
-  }
-  return s
-}
+// const size = str => {
+//   // returns the byte length of an utf8 string
+//   var s = str.length
+//   for (var i= str.length - 1; i >= 0; i--) {
+//     var code = str.charCodeAt(i)
+//     if (code > 0x7f && code <= 0x7ff) { s++ }
+//     else if (code > 0x7ff && code <= 0xffff) { s += 2 }
+//     if (code >= 0xDC00 && code <= 0xDFFF) i-- // trail surrogate
+//   }
+//   return s
+// }
 
 const progress = (client) => {
   if (!client.inProgress) {
