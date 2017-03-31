45163a824aad5a998f793730992a7309cf471d77
Jim de Beer <jim@vigour.io>
2017-03-31 09:23:59 +0200

    try more

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
 require('./connect')
-require('./multiple')
-require('./data-size')
-require('./references')
-require('./subscription')
-require('./switch')
-require('./client')
-require('./types')
-require('./context')
+// require('./multiple')
+// require('./data-size')
+// require('./references')
+// require('./subscription')
+// require('./switch')
+// require('./client')
+// require('./types')
+// require('./context')
 
 // missing auto remove of context
 setTimeout(() => {
