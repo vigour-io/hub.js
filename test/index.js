affd62e20724fd0599f9735bdf6dd9c0e57856e1
Jim de Beer <jim@vigour.io>
2017-03-31 09:27:54 +0200

    re-enable tests

--- test/index.js
+++ test/index.js
@@ -1,14 +1,9 @@
 require('./connect')
-// require('./multiple')
-// require('./data-size')
-// require('./references')
-// require('./subscription')
-// require('./switch')
-// require('./client')
-// require('./types')
-// require('./context')
-
-// missing auto remove of context
-setTimeout(() => {
-  console.log('force a wait.. or something?')
-}, 15e3)
+require('./multiple')
+require('./data-size')
+require('./references')
+require('./subscription')
+require('./switch')
+require('./client')
+require('./types')
+require('./context')
