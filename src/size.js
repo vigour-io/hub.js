19f41df9860f46957669f15571eaaf9bc7f3fd54
Jim de Beer <jim@vigour.io>
2017-03-31 08:49:14 +0200

    next tick for large sizes

--- src/size.js
+++ src/size.js
@@ -1,5 +1,10 @@
 const maxSize = 1e7
 
+const send = (client, payload, next) => {
+  client.socket.send(payload)
+  process.nextTick(next)
+}
+
 const sendLarge = (raw, client) => {
   const size = Buffer.byteLength(raw, 'utf8')
   if (size > maxSize) {
@@ -10,10 +15,28 @@ const sendLarge = (raw, client) => {
     const buf = Buffer.from(raw, 'utf8')
     let i = 0
     // make sure you end with an non maxsize buffer
-    while (i * maxSize <= size) {
-      client.socket.send(buf.slice(i * maxSize, (i + 1) * maxSize))
-      i++
+
+    const drainInProgress = done => {
+      if (client.blobInProgress.length > 0) {
+        send(client, client.blobInProgress.shift(), drainInProgress)
+      } else {
+        done()
+      }
     }
+
+    const next = () => {
+      i++
+      if (i * maxSize <= size) {
+        send(client, buf.slice(i * maxSize, (i + 1) * maxSize), next)
+      } else {
+        drainInProgress(() => {
+          client.blobInProgress = false
+        })
+      }
+    }
+
+    send(client, buf.slice(i * maxSize, (i + 1) * maxSize), next)
+
     return true
   }
 }
@@ -25,7 +48,6 @@ const receiveLarge = (data, bufferArray) => {
 }
 
 export {
-  maxSize,
   sendLarge,
   receiveLarge
 }
