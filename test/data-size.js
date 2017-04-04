d0a12e8badbe70f817ffec07cb951b494f6e555b
Jim de Beer <jim@vigour.io>
2017-04-04 16:20:42 +0200

    make d-size test smaller

--- test/data-size.js
+++ test/data-size.js
@@ -41,7 +41,7 @@ test('data size - from server', { timeout: 6000 }, t => {
     'propriae periculis adversarium eos ex. Insolens percipitur efficiantur qui at, dicam ' +
     'qualisque appellantur ne per. In saepe delenit incorrupte eam, antiopam elaboraret id ' +
     'eum. Mazim noluisse definitiones has ad, vel id erat equidem. Ex qui inani iusto delenit.'
-  let i = 5e3
+  let i = 1e3
   while (i--) {
     const d = 1e11 + Math.round(Math.random() * 1e9) + i
     someData[`key-${d}-longer-string-${d}`] = {
