b95c3b773c33910c4b773e63211ca9f36f186127
Jim de Beer <jim@vigour.io>
2017-04-05 11:26:56 +0200

    start of upstream guard

--- test/subscription.js
+++ test/subscription.js
@@ -33,3 +33,36 @@ test('subscription - val + fields', t => {
 
   client.subscribe({ a: true })
 })
+
+test('subscription - reuse', t => {
+  const server = hub({
+    _uid_: 'server',
+    port: 6060,
+    a: {
+      val: 'a',
+      b: { c: 'c!' }
+    }
+  })
+
+  server.set({ nostamp: 'nostamp!' }, false)
+
+  const client = hub({
+    _uid_: 'client',
+    url: 'ws://localhost:6060'
+  })
+
+  Promise.all([
+    client.get([ 'a', 'b', 'c' ], {}).once('c!'),
+    client.get([ 'a' ], {}).once('a')
+  ]).then(() => {
+    client.subscribe({ nostamp: true })
+    return client.get('nostamp', {}).once('nostamp!')
+  }).then(() => {
+    t.pass('received correct payload')
+    client.set(null)
+    server.set(null)
+    t.end()
+  })
+
+  client.subscribe({ a: true })
+})
