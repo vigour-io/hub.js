5e03760229a3b21b532306cf4332c8cd142073ca
Mustafa Dokumaci <mustafa@foraa.com>
2017-04-12 16:29:35 +0200

    you shall fail

--- test/references.js
+++ test/references.js
@@ -28,71 +28,71 @@ test('references', t => {
   })
 })
 
-// test('circular references', t => {
-//   const scraper = hub({
-//     _uid_: 'scraper',
-//     port: 6060,
-//     a: {
-//       itemList: {
-//         type: 'List',
-//         items: {
-//           b: {
-//             type: 'Item',
-//             val: ['@', 'root', 'b']
-//           },
-//           c: {
-//             type: 'Item',
-//             val: ['@', 'root', 'c']
-//           }
-//         }
-//       }
-//     },
-//     b: {
-//       val: 'valB',
-//       siblings: ['@', 'root', 'a', 'itemList'],
-//       other: {
-//         type: 'other',
-//         val: ['@', 'parent', 'otherData']
-//       },
-//       otherData: 'someText'
-//     },
-//     c: {
-//       val: 'valC',
-//       siblings: ['@', 'root', 'a', 'itemList']
-//     }
-//   })
+test('circular references', t => {
+  const scraper = hub({
+    _uid_: 'scraper',
+    port: 6060,
+    a: {
+      itemList: {
+        type: 'List',
+        items: {
+          b: {
+            type: 'Item',
+            val: ['@', 'root', 'b']
+          },
+          c: {
+            type: 'Item',
+            val: ['@', 'root', 'c']
+          }
+        }
+      }
+    },
+    b: {
+      val: 'valB',
+      siblings: ['@', 'root', 'a', 'itemList'],
+      other: {
+        type: 'other',
+        val: ['@', 'parent', 'otherData']
+      },
+      otherData: 'someText'
+    },
+    c: {
+      val: 'valC',
+      siblings: ['@', 'root', 'a', 'itemList']
+    }
+  })
 
-//   const hybrid = hub({
-//     _uid_: 'hybrid',
-//     url: 'ws://localhost:6060',
-//     port: 6161
-//   })
+  const hybrid = hub({
+    _uid_: 'hybrid',
+    url: 'ws://localhost:6060',
+    port: 6161
+  })
 
-//   hybrid.subscribe(true)
+  hybrid.subscribe(true)
 
-//   const client = hub({
-//     _uid_: 'client',
-//     url: 'ws://localhost:6161',
-//     context: 'someContextKey'
-//   })
+  const client = hub({
+    _uid_: 'client',
+    url: 'ws://localhost:6161',
+    context: 'someContextKey'
+  })
 
-//   client.subscribe({
-//     a: {
-//       itemList: {
-//         $any: {
-//           val: true
-//         }
-//       }
-//     }
-//   })
+  client.subscribe({
+    a: {
+      itemList: {
+        $any: {
+          val: true
+        }
+      }
+    }
+  })
 
-//   setTimeout(() => {
-//     client.get([ 'a', 'itemList', 'items', 'c' ], {}).once('valC').then(() => {
-//       t.pass('received circular reference')
-//       client.set(null)
-//       hybrid.set(null)
-//       scraper.set(null)
-//       t.end()
-//     })
-//   }, 100)
-// })
+  setTimeout(() => {
+    client.get([ 'a', 'itemList', 'items', 'c' ], {}).once('valC').then(() => {
+      t.pass('received circular reference')
+      client.set(null)
+      hybrid.set(null)
+      scraper.set(null)
+      t.end()
+    })
+  }, 100)
+})
