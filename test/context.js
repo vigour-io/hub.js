725f03f628ccac17fcc1f803b16cc64636eb63a6
Jim de Beer <jim@vigour.io>
2017-05-30 17:49:46 +0200

    added fix

--- test/context.js
+++ test/context.js
@@ -1,193 +1,193 @@
 const hub = require('../')
 const test = require('tape')
 
-// test('context', { timeout: 2000 }, t => {
-//   const scraper = hub({
-//     _uid_: 'scraper',
-//     port: 6060,
-//     somefield: 'somefield!'
-//   })
+test('context', { timeout: 2000 }, t => {
+  const scraper = hub({
+    _uid_: 'scraper',
+    port: 6060,
+    somefield: 'somefield!'
+  })
 
-//   const hybrid = hub({
-//     _uid_: 'hybrid',
-//     url: 'ws://localhost:6060',
-//     port: 6061
-//   })
+  const hybrid = hub({
+    _uid_: 'hybrid',
+    url: 'ws://localhost:6060',
+    port: 6061
+  })
 
-//   hybrid.subscribe(true)
+  hybrid.subscribe(true)
 
-//   const client1 = hub({
-//     _uid_: 'client1',
-//     context: 'pavel',
-//     url: 'ws://localhost:6061'
-//   })
+  const client1 = hub({
+    _uid_: 'client1',
+    context: 'pavel',
+    url: 'ws://localhost:6061'
+  })
 
-//   const client2 = hub({
-//     _uid_: 'client2',
-//     context: 'pavel',
-//     url: 'ws://localhost:6061'
-//   })
+  const client2 = hub({
+    _uid_: 'client2',
+    context: 'pavel',
+    url: 'ws://localhost:6061'
+  })
 
-//   const client3 = hub({
-//     _uid_: 'client3',
-//     url: 'ws://localhost:6061'
-//   })
+  const client3 = hub({
+    _uid_: 'client3',
+    url: 'ws://localhost:6061'
+  })
 
-//   const client4 = hub({
-//     _uid_: 'client4',
-//     url: 'ws://localhost:6060'
-//   })
+  const client4 = hub({
+    _uid_: 'client4',
+    url: 'ws://localhost:6060'
+  })
 
-//   client1.subscribe(true)
-//   client2.subscribe(true)
-//   client3.subscribe(true)
+  client1.subscribe(true)
+  client2.subscribe(true)
+  client3.subscribe(true)
 
-//   Promise.all([
-//     client2.get('blurf', {}).once('hello'),
-//     client2.get('somefield', {}).once('somefield!'),
-//     client1.get('somefield', {}).once('somefield!')
-//   ]).then(() => {
-//     t.pass('client2 recieves correct value')
-//     t.pass('client2 receives server-1 somefield!')
-//     client3.set({ somefield: 'hahaha' })
-//   })
+  Promise.all([
+    client2.get('blurf', {}).once('hello'),
+    client2.get('somefield', {}).once('somefield!'),
+    client1.get('somefield', {}).once('somefield!')
+  ]).then(() => {
+    t.pass('client2 recieves correct value')
+    t.pass('client2 receives server-1 somefield!')
+    client3.set({ somefield: 'hahaha' })
+  })
 
-//   Promise.all([
-//     client1.get('somefield', {}).once('hahaha'),
-//     client2.get('somefield', {}).once('hahaha')
-//   ]).then(() => {
-//     t.pass('client1 & client2 receive context updates')
-//     client4.set({ smurf: true })
-//   })
+  Promise.all([
+    client1.get('somefield', {}).once('hahaha'),
+    client2.get('somefield', {}).once('hahaha')
+  ]).then(() => {
+    t.pass('client1 & client2 receive context updates')
+    client4.set({ smurf: true })
+  })
 
-//   Promise.all([
-//     client1.get('smurf', {}).once(true),
-//     client2.get('smurf', {}).once(true),
-//     client3.get('smurf', {}).once(true)
-//   ]).then(() => {
-//     t.pass('client1 & client2 & client3 receive updates')
-//     client3.get('blurf', {}).once('hello').then(() => {
-//       t.pass('client3 receives updates after switching context')
-//       client1.set({ context: false })
-//       hybrid.getContext('pavel').clients.once(t => t.keys().length === 2)
-//       .then(() => {
-//         t.pass('removed client from hybrid')
-//         client1.set(null)
-//         client2.set(null)
-//         client3.set(null)
-//         client4.set(null)
-//         hybrid.set(null)
-//         scraper.set(null)
-//         t.end()
-//       })
-//     })
-//     client3.set({ context: 'pavel' })
-//   })
+  Promise.all([
+    client1.get('smurf', {}).once(true),
+    client2.get('smurf', {}).once(true),
+    client3.get('smurf', {}).once(true)
+  ]).then(() => {
+    t.pass('client1 & client2 & client3 receive updates')
+    client3.get('blurf', {}).once('hello').then(() => {
+      t.pass('client3 receives updates after switching context')
+      client1.set({ context: false })
+      hybrid.getContext('pavel').clients.once(t => t.keys().length === 2)
+      .then(() => {
+        t.pass('removed client from hybrid')
+        client1.set(null)
+        client2.set(null)
+        client3.set(null)
+        client4.set(null)
+        hybrid.set(null)
+        scraper.set(null)
+        t.end()
+      })
+    })
+    client3.set({ context: 'pavel' })
+  })
 
-//   client1.set({ blurf: 'hello' })
-// })
+  client1.set({ blurf: 'hello' })
+})
 
-// test('context - getContext - error', { timeout: 2000 }, t => {
-//   const server = hub({
-//     _uid_: 'server',
-//     getContext: (context, retrieve, hub, socket) => new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         reject(new Error('💩'))
-//       }, 100)
-//     }),
-//     port: 6060
-//   })
-//   server.on('error', () => {
-//     t.equal(server.instances, void 0)
-//     client.set(null)
-//     server.set(null)
-//     t.end()
-//   })
-//   const client = hub({
-//     _uid_: 'client1',
-//     context: 'pavel',
-//     url: 'ws://localhost:6060'
-//   })
-// })
+test('context - getContext - error', { timeout: 2000 }, t => {
+  const server = hub({
+    _uid_: 'server',
+    getContext: (context, retrieve, hub, socket) => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(new Error('💩'))
+      }, 100)
+    }),
+    port: 6060
+  })
+  server.on('error', () => {
+    t.equal(server.instances, void 0)
+    client.set(null)
+    server.set(null)
+    t.end()
+  })
+  const client = hub({
+    _uid_: 'client1',
+    context: 'pavel',
+    url: 'ws://localhost:6060'
+  })
+})
 
-// test('context - getContext', { timeout: 2000 }, t => {
-//   const server = hub({
-//     _uid_: 'server',
-//     getContext: (context, retrieve, hub, socket) => new Promise((resolve, reject) => {
-//       setTimeout(() => {
-//         resolve(hub)
-//       }, 100)
-//     }),
-//     port: 6060
-//   })
+test('context - getContext', { timeout: 2000 }, t => {
+  const server = hub({
+    _uid_: 'server',
+    getContext: (context, retrieve, hub, socket) => new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(hub)
+      }, 100)
+    }),
+    port: 6060
+  })
 
-//   const client2 = hub({
-//     _uid_: 'client2',
-//     context: 'pavel',
-//     url: 'ws://localhost:6060'
-//   })
+  const client2 = hub({
+    _uid_: 'client2',
+    context: 'pavel',
+    url: 'ws://localhost:6060'
+  })
 
-//   client2.set({ x: 'yes' })
+  client2.set({ x: 'yes' })
 
-//   const client = hub({
-//     _uid_: 'client1',
-//     context: 'pavel',
-//     url: 'ws://localhost:6060'
-//   })
+  const client = hub({
+    _uid_: 'client1',
+    context: 'pavel',
+    url: 'ws://localhost:6060'
+  })
 
-//   client.subscribe({ x: true })
+  client.subscribe({ x: true })
 
-//   client.get('x', {}).once('yes').then(() => {
-//     client.set(null)
-//     client2.set(null)
-//     server.set(null)
-//     t.end()
-//   })
-// })
+  client.get('x', {}).once('yes').then(() => {
+    client.set(null)
+    client2.set(null)
+    server.set(null)
+    t.end()
+  })
+})
 
-// test('context - basic', { timeout: 2000 }, t => {
-//   const scraper = hub({
-//     _uid_: 'scraper',
-//     port: 6060,
-//     somefield: 'somefield!'
-//   })
+test('context - basic', { timeout: 2000 }, t => {
+  const scraper = hub({
+    _uid_: 'scraper',
+    port: 6060,
+    somefield: 'somefield!'
+  })
 
-//   const hybrid = hub({
-//     _uid_: 'hybrid',
-//     url: 'ws://localhost:6060',
-//     port: 6061
-//   })
+  const hybrid = hub({
+    _uid_: 'hybrid',
+    url: 'ws://localhost:6060',
+    port: 6061
+  })
 
-//   hybrid.subscribe(true)
+  hybrid.subscribe(true)
 
-//   const client2 = hub({
-//     _uid_: 'client2',
-//     context: 'pavel',
-//     url: 'ws://localhost:6061'
-//   })
+  const client2 = hub({
+    _uid_: 'client2',
+    context: 'pavel',
+    url: 'ws://localhost:6061'
+  })
 
-//   const client = hub({
-//     _uid_: 'client1',
-//     context: 'pavelx',
-//     url: 'ws://localhost:6061'
-//   })
+  const client = hub({
+    _uid_: 'client1',
+    context: 'pavelx',
+    url: 'ws://localhost:6061'
+  })
 
-//   client.subscribe({ x: true })
-//   client2.subscribe({ x: true })
+  client.subscribe({ x: true })
+  client2.subscribe({ x: true })
 
-//   scraper.set({ x: 'x' })
+  scraper.set({ x: 'x' })
 
-//   Promise.all([
-//     client.get('x', {}).once('x'),
-//     client2.get('x', {}).once('x')
-//   ]).then(() => {
-//     scraper.set(null)
-//     hybrid.set(null)
-//     client.set(null)
-//     client2.set(null)
-//     t.end()
-//   })
-// })
+  Promise.all([
+    client.get('x', {}).once('x'),
+    client2.get('x', {}).once('x')
+  ]).then(() => {
+    scraper.set(null)
+    hybrid.set(null)
+    client.set(null)
+    client2.set(null)
+    t.end()
+  })
+})
 
 test('context - fire subscriptions on switch context', { timeout: 2000 }, t => {
   const server = hub({
