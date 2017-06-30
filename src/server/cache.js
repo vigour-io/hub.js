2c3fbc5ebb2b7f9fd41efdbcbb6a6333327fe7d6
Mustafa Dokumaci <mustafa@foraa.com>
2017-06-30 18:21:19 +0200

    shared path list

--- src/server/cache.js
+++ src/server/cache.js
@@ -2,10 +2,15 @@ import { puid } from 'brisky-struct'
 
 const contextBit = 0b100000000000000000000000000000000000000000000
 const contextBitNegative = 0b1100000000000000000000000000000000000000000000
+const pathList = {}
 
 const cache = (client, struct, hub, stamp) => {
   if (!client.cache) client.cache = {}
-  client.cache[puid(struct)] = hub.contextKey === undefined ? stamp
+  const uid = puid(struct)
+  if (!pathList[uid]) {
+    pathList[uid] = struct.path()
+  }
+  client.cache[uid] = hub.contextKey === undefined ? stamp
     : stamp < 0 ? -stamp | contextBitNegative
     : stamp | contextBit
 }
