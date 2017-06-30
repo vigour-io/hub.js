8779bd08c450467d24ea5ab90672dc3bfeddcc04
Mustafa Dokumaci <mustafa@foraa.com>
2017-06-30 16:28:10 +0200

    negative fix

--- src/server/cache.js
+++ src/server/cache.js
@@ -1,17 +1,18 @@
 import { puid } from 'brisky-struct'
 
+const contextBit = 0b100000000000000000000000000000000000000000000
+const contextBitNegative = 0b1100000000000000000000000000000000000000000000
+
 const cache = (client, struct, hub, stamp) => {
   if (!client.cache) client.cache = {}
-//  client.cache[puid(struct)] = hub.contextKey === undefined ? stamp
-//    : stamp | contextBit
-  client.cache[puid(struct)] = stamp
+  client.cache[puid(struct)] = hub.contextKey === undefined ? stamp
+    : stamp < 0 ? -stamp | contextBitNegative
+    : stamp | contextBit
 }
 
-//  const isCached = (client, struct, hub, stamp) => client.cache &&
-//    client.cache[puid(struct)] === hub.contextKey === undefined ? stamp
-//    : stamp | contextBit
-
-const isCached = (client, struct, stamp) => client.cache &&
-  client.cache[puid(struct)] === stamp
+const isCached = (client, struct, hub, stamp) => client.cache &&
+  client.cache[puid(struct)] === hub.contextKey === undefined ? stamp
+    : stamp < 0 ? -stamp | contextBitNegative
+    : stamp | contextBit
 
 export { cache, isCached }
