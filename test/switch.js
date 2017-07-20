1532df99e3ea1fecdfcf7414df2f169bd055a426
Mustafa Dokumaci <mustafa@foraa.com>
2017-07-20 14:57:10 +0200

    fix switch test

--- test/switch.js
+++ test/switch.js
@@ -1,73 +1,137 @@
 const hub = require('../')
 const test = require('tape')
 
-test('switch', { timeout: 500 }, t => {
-  t.plan(6)
+test('switch', { timeout: 1e3 }, t => {
+  t.plan(11)
 
   const server = hub({
     _uid_: 'server',
     port: 6061,
-    key: 'server',
     pageA: {
-      fixedA: 'dataA'
+      itemsA: {
+        itemA1: {
+          val: ['@', 'root', 'items', 'item1'],
+          rField: 'rVal'
+        },
+        itemA2: ['@', 'root', 'items', 'item2']
+      }
     },
     pageB: {
-      fixedB: 'dataB'
+      itemsB: {
+        itemB1: ['@', 'root', 'items', 'item3']
+      }
+    },
+    items: {
+      item1: {
+        field1: 'val1'
+      },
+      item2: {
+        field2: 'val2'
+      },
+      item3: {
+        field3: 'val3'
+      }
     }
-  })
-
-  server.on('error', err => {
-    console.log('server error', err)
   })
 
   const client = hub({
     url: 'ws://localhost:6061',
     _uid_: 'client',
-    context: 'first'
+    context: 'first',
+    ref: ['@', 'parent', 'pageA']
   })
 
   client.subscribe({
     ref: {
       $switch: t => {
-        return t.origin().key === 'pageA' ? {
-          fixedA: { val: true }
-        } : {
-          fixedB: { val: true }
-        }
+        return t.origin().key === 'pageA' ? { itemsA: { val: true } }
+        : { itemsB: { val: true } }
       }
     }
   }, (val, type) => {
-    if ((type === 'new' || type === 'update') && val.val) {
-      t.equal(
-        client.get(['ref', 'origin', 'key']),
-        val.get(['parent', 'key']),
-        `subscription fired correctly for ${val.parent().key}`
+    if ((type === 'new' || type === 'update') && val.key === 'itemsA') {
+      if (val.get(['itemA1', 'focus'])) {
+        t.same(val.serialize(), {
+          itemA1: {
+            val: ['@', 'root', 'items', 'item1'],
+            rField: 'rVal',
+            focus: true
+          },
+          itemA2: ['@', 'root', 'items', 'item2']
+        }, 'itemsA fired correctly with focus')
+      } else {
+        t.same(val.serialize(), {
+          itemA1: { rField: 'rVal', val: ['@', 'root', 'items', 'item1'] },
+          itemA2: ['@', 'root', 'items', 'item2']
+        }, 'itemsA fired correctly without focus')
+      }
+    } else if (type === 'new' && val.key === 'itemsB') {
+      t.same(
+        val.serialize(), { itemB1: ['@', 'root', 'items', 'item3'] },
+        'itemsB fired correctly'
       )
     }
-    console.log(val, type)
   })
 
-  client.set({ ref: [ '@', 'parent', 'pageA' ] })
-
-  client.get([ 'pageA', 'fixedA' ], {}).once('dataA')
+  client.get(['items', 'item1', 'field1'], {}).once('val1')
     .then(() => {
-      t.pass('switched to A')
+      t.equals(
+        client.get(['ref', 'itemsA', 'itemA1', 'rField', 'compute']), 'rVal',
+        'itemA1 got rField'
+      )
+      t.equals(
+        client.get(['ref', 'itemsA', 'itemA1', 'field1', 'compute']), 'val1',
+        'itemA1 got field1'
+      )
+      t.equals(
+        client.get(['ref', 'itemsA', 'itemA2', 'field2', 'compute']), 'val2',
+        'itemA2 got field2'
+      )
+
+      client.set({
+        pageA: {
+          itemsA: {
+            itemA1: {
+              focus: {
+                val: true
+              }
+            }
+          }
+        }
+      })
+
       client.set({ context: 'second' })
 
-      // TODO: fix subscriptions to avoid this timeout
       setTimeout(() => {
         client.set({ ref: [ '@', 'parent', 'pageB' ] })
       }, 50)
 
-      return client.get([ 'pageB', 'fixedB' ], {}).once('dataB')
+      return client.get(['items', 'item3', 'field3'], {}).once('val3')
     })
     .then(() => {
-      t.pass('switched to B')
+      t.equals(
+        client.get(['ref', 'itemsB', 'itemB1', 'field3', 'compute']), 'val3',
+        'itemB1 got field3'
+      )
+
       client.set({ ref: [ '@', 'parent', 'pageA' ] })
-      return client.get([ 'pageA', 'fixedA' ], {}).once('dataA')
+
+      return client.get(['pageA', 'itemsA', 'itemA1', 'field1']).once('val1')
     })
     .then(() => {
-      t.pass('switched back to A')
+      t.equals(
+        client.get(['ref', 'itemsA', 'itemA1', 'rField', 'compute']), 'rVal',
+        'itemA1 got rField'
+      )
+      t.equals(
+        client.get(['ref', 'itemsA', 'itemA1', 'field1', 'compute']), 'val1',
+        'itemA1 got field1'
+      )
+      t.equals(
+        client.get(['ref', 'itemsA', 'itemA2', 'field2', 'compute']), 'val2',
+        'itemA2 got field2'
+      )
+
       server.set(null)
       client.set(null)
     })
