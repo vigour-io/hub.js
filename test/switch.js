c178bc5656315cc2226a9b8b5dc2d7c9c17ffef8
Jim de Beer <jim@vigour.io>
2017-07-28 16:33:00 +0200

    pass tests

--- test/switch.js
+++ test/switch.js
@@ -2,6 +2,51 @@ const hub = require('../')
 const test = require('tape')
 const { puid } = require('brisky-struct')
 
+test('switch', { timeout: 1e3 }, t => {
+  const server = hub({
+    _uid_: 'server',
+    key: 'server',
+    port: 6061,
+    bla: {
+      a: 'hello'
+    },
+    blurf: {
+      b: 'hello'
+    }
+  })
+
+  server.on('error', err => {
+    console.log('server error', err)
+  })
+
+  const client = hub({
+    url: 'ws://localhost:6061',
+    _uid_: 'client1'
+    // context: 'a'
+  })
+
+  client.subscribe({
+    ref: {
+      $switch: t => {
+        return t.origin().key === 'blurf' ? {
+          b: { val: true }
+        } : {
+          a: { val: true }
+        }
+      }
+    }
+  })
+
+  client.set({ ref: [ '@', 'parent', 'blurf' ] })
+
+  client.get([ 'blurf', 'b' ], {}).once('hello').then(() => {
+    t.pass('received blurf.b')
+    server.set(null)
+    client.set(null)
+    t.end()
+  })
+})
+
 // test('switch - basic', { timeout: 1e3 }, t => {
 //   console.log('wtf!')
 //   t.plan(1)
